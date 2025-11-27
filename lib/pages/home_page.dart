c64c1eba1fb0f404baa20003f29eaea7687ea8ef
jorabekisoqov <j.isoqov@newuu.uz>
2025-11-27 09:56:19 +0500

    project have been updated

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:../firebase_options.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        centerTitle: false,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              // open profile page later
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.grey.shade300,
+                child: const Icon(Icons.person, color: Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          user != null
+              ? "Welcome, ${user.displayName ?? user.email ?? "User"}"
+              : "You're not logged in",
+          style: const TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
